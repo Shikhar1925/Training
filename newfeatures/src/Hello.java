a3aaa011056958f24150cca0e39e3a79a93bb8f2
AZ581YB <AZ581YB@IN3480582W1.ey.net>
2023-09-04 15:52:17 +0530

    New Basic Logic-Programs

---
+++ newfeatures/src/Hello.java
@@ -0,0 +1,13 @@
+@FunctionalInterface
+public interface Hello {
+	String sayHello();
+	
+	 default String sayBye() {
+		return "Good Bye!";
+	}
+	 
+	 static void greeting() {
+		 System.out.println("Good Day");
+	 }
+
+}
